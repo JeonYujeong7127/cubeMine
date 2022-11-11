63701d4415daa5fe6b4af09f1e0dd291c9cde7b5
hhyy0179 <hhyy0179@gmail.com>
2022-11-11 23:47:57 +0900

    체력bar 생성, 캐릭터 모션, 아이템 충돌 및 파티클

--- CubeMine/Source/CubeMine/CubeMine.Build.cs
+++ CubeMine/Source/CubeMine/CubeMine.Build.cs
@@ -8,6 +8,8 @@ public class CubeMine : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" , "UMG" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 	}
 }
